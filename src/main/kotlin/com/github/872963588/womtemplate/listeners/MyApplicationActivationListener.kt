c98b39674dd59fa8b5bec34537232b303ceff71f
chengrun <chengrun@supcon.com>
2023-12-25 18:59:46 +0800

    测试

--- src/main/kotlin/com/github/872963588/womtemplate/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/872963588/womtemplate/listeners/MyApplicationActivationListener.kt
@@ -5,8 +5,16 @@ import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.wm.IdeFrame
 
 internal class MyApplicationActivationListener : ApplicationActivationListener {
-
+    companion object {
+        var p: Project? = null
+    }
     override fun applicationActivated(ideFrame: IdeFrame) {
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+        super.applicationActivated(ideFrame)
+        p = ideFrame.project
+        p?.getService(MyProjectService::class.java)
+    }
+    override fun applicationDeactivated(ideFrame: IdeFrame) {
+        super.applicationDeactivated(ideFrame)
+        p = null
     }
 }
--- src/main/kotlin/com/github/872963588/womtemplate/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/872963588/womtemplate/toolWindow/MyToolWindowFactory.kt
@@ -15,9 +15,9 @@ import javax.swing.JButton
 
 class MyToolWindowFactory : ToolWindowFactory {
 
-    init {
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
-    }
+//    init {
+//        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+//    }
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val myToolWindow = MyToolWindow(toolWindow)
